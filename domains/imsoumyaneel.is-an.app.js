46031b494838eda48149451adf2cf16de1dd73c0
Soumyaneel Sarkar <65025579+A-Little-Hat@users.noreply.github.com>
2024-02-22 02:22:22 +0530

    Create imsoumyaneel.is-an.app.js

---
+++ domains/imsoumyaneel.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'portfolio project',
+  domain: 'is-an.app',
+  subdomain: 'imsoumyaneel',
+  owner: {
+    repo: 'https://github.com/A-Little-Hat/A-Little-Hat',
+    email: 'soumyasarkar309@gmail.com',
+  },
+  record: {
+    CNAME: 'a-little-hat.github.io',
+  },
+  proxy: false,
+})
